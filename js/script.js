c44446874425b9f0e8e3a5bef5e2f280da2e67d1
Ara Noreen <aracorsiga@gmail.com>
2017-10-13 13:22:15 +0800

    added new file on index.html, css/style.css and js/script.js

--- js/script.js
+++ js/script.js
@@ -25,8 +25,27 @@ function nextTurn() {
 	setMessage("It's " + document.turn + "'s turn.");
 }
 
-function checkRow(a,b,c, move) {
+function checkForWinner(move) {
+	var result = false;
+	if (checkRow(1, 2, 3, move) ||
+		checkRow(4, 5, 6, move) ||
+		checkRow(7, 8, 9, move) ||
+		checkRow(1, 4, 7, move) ||
+		checkRow(2, 5, 8, move) ||
+		checkRow(3, 6, 9, move) ||
+		checkRow(1, 5, 9, move) ||
+		checkRow(3, 5, 7, move)) || {
+		result = true;
+	}
+	return result;
+}
 
+function checkRow(a,b,c, move) {
+	var result = false;
+	if (getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+		result = true;
+	}
+	return result;
 }
 
 function getRow(number) {
